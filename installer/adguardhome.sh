b5bca9be1c729f7734f11ed7194386c4e90e738c
Nico <67909777+nilasystem@users.noreply.github.com>
2022-10-31 23:51:02 +0100

    Initial commit

---
+++ installer/adguardhome.sh
@@ -0,0 +1,38 @@
+#!/bin/bash
+sleep 1
+clear
+echo "############################################################################"
+echo "#                          Adguard Home Installer                          #"
+echo "#                              by Nico L.                                  #"
+echo "#                   https://github.com/nilasystem/requestscripts           #"
+echo "#                          Last Update: 24.09.2022                         #"
+echo "############################################################################"
+sleep 3
+clear
+echo "############################################################################"
+echo "#                         Installing Adguard Home                          #"
+echo "############################################################################"
+sleep 2
+echo "############################################################################"
+echo "#                      Server Update (apt update -y)                       #"
+echo "############################################################################"
+apt update -y
+wait -n
+echo "############################################################################"
+echo "#                      Server Upgrade (apt upgrade -y)                     #"
+echo "############################################################################"
+apt upgrade -y
+wait -n 
+echo "############################################################################"
+echo "#                 cURL Installation (apt install curl -y)                  #"
+echo "############################################################################"
+apt install curl -y
+wait -n 
+echo "###################################################################################################################"
+echo "#                                   Downloading Adguard Home Installer                                            #"
+echo "# curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v #"
+echo "###################################################################################################################"
+curl -s -S -L https://raw.githubusercontent.com/AdguardTeam/AdGuardHome/master/scripts/install.sh | sh -s -- -v
+
+
+
---
+++ installer/keyhelp.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+sleep 1
+clear
+echo "############################################################################"
+echo "#                          Keyhelp Installer                               #"
+echo "#                             by Nico L.                                   #"
+echo "#                 https://github.com/nilasystem/requestscripts             #"
+echo "#                          Last Update: 24.09.2022                         #"
+echo "############################################################################"
+sleep 3
+clear
+echo "############################################################################"
+echo "#                         Installing Keyhelp                               #"
+echo "############################################################################"
+sleep 2
+echo "############################################################################"
+echo "#                      Server Update (apt update -y)                       #"
+echo "############################################################################"
+apt update -y
+wait -n
+echo "############################################################################"
+echo "#                      Server Upgrade (apt upgrade -y)                     #"
+echo "############################################################################"
+apt upgrade -y
+wait -n 
+echo "####################################################################################################"
+echo "#                                     Keyhelp Installation                                         #"
+echo "#wget https://install.keyhelp.de/get_keyhelp.php -O install_keyhelp.sh ; bash install_keyhelp.sh ; #"
+echo "####################################################################################################"
+wget https://install.keyhelp.de/get_keyhelp.php -O install_keyhelp.sh ; bash install_keyhelp.sh ;
+
+
